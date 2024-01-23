d6eca97ab78c84738472d30a32eb6f63291fc98f
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-23 09:25:42 +0500

    Do While Loop

---
+++ tut_22_do_while.java
@@ -0,0 +1,9 @@
+public class tut_22_do_while {
+    public static void main(String[] args) {
+        int number=5;
+        do {
+            System.out.println("Do PART OF DO While loop");
+            number=number-1;
+        } while (number>1);
+    }
+}
