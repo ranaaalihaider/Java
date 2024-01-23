21a45d102665ff1ef733ee05bd6fcf55b159da68
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-23 22:09:25 +0500

    Arrays Introduction

---
+++ tut_26_arrays.java
@@ -0,0 +1,19 @@
+public class tut_26_arrays {
+    public static void main(String[] args) {
+        //now we will studay array 
+        //array is a collection of similar type of data
+        int [] marks; //array named as marks is created
+        marks=new int[5];
+        marks[0]=11;
+        marks[1]=12;
+        marks[2]=13;
+        marks[3]=14;
+        marks[4]=15;
+        System.out.println(marks[3]);
+
+        int [] numbers = {20,21,22,23,24,25,26,27,28,29};
+        System.out.println(numbers[5]);
+
+
+    }
+}
