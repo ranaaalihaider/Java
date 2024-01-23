9a1dbf5a6777e4ead01ea57ef5a8dbe254b01ba0
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-23 09:19:21 +0500

    While loop

---
+++ tut_21_loops.java
@@ -0,0 +1,10 @@
+public class tut_21_loops {
+    public static void main(String[] args) {
+        int number=1;
+        while (number<=10) {
+            System.out.println(number);
+            number=number+1;
+        }
+
+    }
+}
