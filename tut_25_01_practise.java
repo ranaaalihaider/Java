5ab2209486fc9b6690c6c620ff320c63cdcaf26b
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-23 10:24:14 +0500

    Star Pattern

---
+++ tut_25_01_practise.java
@@ -0,0 +1,10 @@
+public class tut_25_01_practise {
+    public static void main(String[] args) {
+        for(int i=4; i>=1; i-- ){
+            for(int j=i; j>=1; j--){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+}
