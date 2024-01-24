f1e9824a8e2df52cab08cbe2fa47b1cc4aea8071
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-25 04:43:13 +0500

    For Each Loop with Arrays

---
+++ tut_27_02_for_each_loop.java
@@ -0,0 +1,8 @@
+public class tut_27_02_for_each_loop {
+    public static void main(String[] args) {
+        String [] students= {"Ali","Haider","Rehman","Rayyan"};
+        for(String names:students){
+            System.out.println(names);
+        }
+    }
+}
