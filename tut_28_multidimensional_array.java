74f788008ee5e72a43b5a17452d9cb9dd05ebdde
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-25 19:45:13 +0500

    Multidimensional Array

---
+++ tut_28_multidimensional_array.java
@@ -0,0 +1,17 @@
+public class tut_28_multidimensional_array {
+    public static void main(String[] args) {
+        int[] [] flats;
+        flats= new int[2][3];
+        flats[0][0]= 101;
+        flats[0][1]= 102;
+        flats[0][2]= 103;
+        flats[1][0]= 201;
+        flats[1][1]= 202;
+        flats[1][2]= 203;
+        for(int i=0; i<=flats.length;i++){
+            for(int k=0; k<flats[i].length;k++)
+            System.out.println(flats[i][k]);
+        }
+
+    }
+}
