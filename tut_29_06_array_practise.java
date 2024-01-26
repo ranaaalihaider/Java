62dab60c22b31942251c833ca164be16d797ae8d
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-26 21:45:06 +0500

    Reversing Array With Swaping Method

---
+++ tut_29_06_array_practise.java
@@ -0,0 +1,19 @@
+public class tut_29_06_array_practise {
+    public static void main(String[] args) {
+        //Reversing Array with  Swaping Method
+        int [] numbers={11,22,33,44,55,66,77,88,99};
+        int n=Math.floorDiv(numbers.length, 2);
+        System.out.println(n);
+        int length=numbers.length;
+        int temp;
+        for(int i=0;i<n;i++){
+            temp=numbers[i];
+            numbers[i]=numbers[length-1-i];
+            numbers[length-1-i]=temp;
+
+        }
+        for(int element:numbers){
+            System.out.println(element);
+        }
+    }
+}
