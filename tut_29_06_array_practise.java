0d3bcd3c9e37c35fcfac425aba1af33756bfbf10
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-26 21:50:01 +0500

    Swapig Arrays

--- tut_29_06_array_practise.java
+++ tut_29_06_array_practise.java
@@ -2,8 +2,12 @@ public class tut_29_06_array_practise {
     public static void main(String[] args) {
         //Reversing Array with  Swaping Method
         int [] numbers={11,22,33,44,55,66,77,88,99};
+        System.out.print("Without Swap Results are : ");
+        for(int element:numbers){
+            System.out.print(element+" ");
+        }
         int n=Math.floorDiv(numbers.length, 2);
-        System.out.println(n);
+        System.out.println("");
         int length=numbers.length;
         int temp;
         for(int i=0;i<n;i++){
@@ -12,8 +16,9 @@ public class tut_29_06_array_practise {
             numbers[length-1-i]=temp;
 
         }
+        System.out.print("With Swap Results are :    ");
         for(int element:numbers){
-            System.out.println(element);
+            System.out.print(element+" ");
         }
     }
 }
