8e6fd9b1943d8827fed3843c6df9ff133e047cc9
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-26 21:28:03 +0500

    Reverse Array with Loop

---
+++ tut_29_05_array_practise.java
@@ -0,0 +1,9 @@
+public class tut_29_05_array_practise {
+    public static void main(String[] args) {
+        //Reversing array
+        int [] numbers={11,22,33,44,55,66,77,88,99};
+        for(int i=(numbers.length-1);i>=0;i--){
+            System.out.print(numbers[i]+" ");
+        }
+    }
+}
