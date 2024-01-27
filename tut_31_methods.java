d1f2f5c2f11e0d3e07b2e9dce7fed5ac3949cbed
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-27 23:03:39 +0500

    Methods in Java

---
+++ tut_31_methods.java
@@ -0,0 +1,15 @@
+public class tut_31_methods {
+    static int logic(int x,int y ){
+        int z;
+        z=x+y;
+        return z;
+    }
+    public static void main(String[] args) {
+    int a=5;
+    int b=10;
+
+    int result=logic(a, b);
+    System.out.println(result);
+    
+    }
+}
