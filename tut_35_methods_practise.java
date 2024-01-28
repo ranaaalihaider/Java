c38065ec8c02f731df104a485e37fc2bb2a1e10e
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-28 23:50:50 +0500

    Methods Pracrise

---
+++ tut_35_methods_practise.java
@@ -0,0 +1,15 @@
+public class tut_35_methods_practise {
+    //Method for Multiplication table
+    static void table(int number){
+        for(int i=1;i<=10;i++){
+            System.out.println(number+" X "+i+" = "+(i*number));
+        }
+    }
+    public static void main(String[] args) {
+        //Multiplication table
+        table(5);
+
+
+    }
+    
+}
