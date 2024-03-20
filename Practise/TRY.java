2f6474da50cad6fca5e254d21d655b4e8273198e
Ranaa Ali Haider <135860810+ranaaalihaider@users.noreply.github.com>
2024-03-20 20:59:20 +0500

    Lab_06

---
+++ Practise/TRY.java
@@ -0,0 +1,7 @@
+public class TRY {
+    public static void main(String[] args) {
+        String demo="COMSATS";
+        demo=demo.replace('S', 'Q');
+        System.out.println(demo);
+    }
+}
--- Practise/tempCodeRunnerFile.java
+++ Practise/tempCodeRunnerFile.java
@@ -1,1 +1,8 @@
-            if (checker==true)
+        double x1=39.55;
+        double x2=41.5;
+        double y1=-116.25;
+        double y2=87.37;
+        double radius=6371000.01;
+        double d=radius * Math.cos(((Math.sin(x1))*(Math.sin(x2)))+((Math.cos(x1))*(Math.cos(x2))*(Math.cos(y1-y2))));
+        System.out.println(d);
+        
