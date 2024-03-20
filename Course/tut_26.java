28108dd0e83a0e5a72c0f139aa732b46e3ad4f88
Ranaa Ali Haider <135860810+ranaaalihaider@users.noreply.github.com>
2024-03-21 00:32:59 +0500

    Course

---
+++ Course/tut_26.java
@@ -0,0 +1,18 @@
+public class tut_26 {
+    public static void main(String[] args) {
+        //declaring array of 5 length
+        int [] marks = new int [5];
+        //initializing array
+
+        marks [0]= 10;
+        marks [1]= 20;
+        marks [2]= 30;
+        marks [3]= 40;
+        marks [4]= 50;
+        System.out.println(marks[0]);
+        //declaring and initialing in same line and length is not specific
+        int [] marks2={98,23,45,56,23,32,546,23,};
+        System.out.println(marks2[1]);
+
+    }
+}
