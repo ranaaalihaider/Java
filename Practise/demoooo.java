584729ef91534bf1fd259b32cf2cf0a766e46dbc
Ranaa Ali Haider <135860810+ranaaalihaider@users.noreply.github.com>
2024-03-21 00:33:08 +0500

    Lab

---
+++ Practise/demoooo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class demoooo 
+{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String let=input.next();
+        String letter=let;
+        if (letter=="A")
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("Falls");
+        }
+    }
+}
