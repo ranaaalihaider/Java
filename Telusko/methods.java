4705767ae7badb92d30acd46c3070980ffa84dbe
Ranaa Ali Haider <135860810+ranaaalihaider@users.noreply.github.com>
2024-03-26 23:14:51 +0500

    Random Codes

---
+++ Telusko/methods.java
@@ -0,0 +1,23 @@
+class Calculator
+{
+    static int num=5;
+    public int add(int n1,int n2)
+    {
+        
+        return n1+n2;
+    }
+}
+public class methods 
+{
+    public static void main(String[] args) 
+    {
+        int data =10;
+        Calculator obj=new Calculator();
+        Calculator obj1=new Calculator();
+        int r1=obj.add(3,4);
+        System.out.println(r1);
+        obj1.num+=4;
+        System.out.println(obj.num);
+        System.out.println(obj1.num);
+    }
+}
