6979780eeff5bd28a944c72822a349fc773cd2f1
Ranaa Ali Haider <135860810+ranaaalihaider@users.noreply.github.com>
2024-04-04 02:56:06 +0500

    Assignment

---
+++ Assignment/Assignment_02/Assignment_02/src/main/java/com/mycompany/assignment_02/Assignment_02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment_02;
+
+/**
+ *
+ * @author Ranaa
+ */
+public class Assignment_02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Assignment/Assignment_02/Assignment_02/src/main/java/com/mycompany/assignment_02/Task_05_B.java
@@ -0,0 +1,21 @@
+package com.mycompany.assignment_02;
+
+public class Task_05_B
+{
+    public static void main(String[] args) 
+    {
+        pattern();
+    }
+    public static void pattern ()
+    {
+        for(int i=6; i>=1; i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.printf("%-2d",j);
+            }
+            System.out.println("");
+        }
+    }
+}
+
